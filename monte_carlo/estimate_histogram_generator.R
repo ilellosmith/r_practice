1066dc562f9b68e6b46b7868d308bcacdb438201
Isaac Lello-Smith <35873591+ilellosmith@users.noreply.github.com>
2020-04-17 23:04:17 -0400

    adding function definitions, doc

--- monte_carlo/estimate_histogram_generator.R
+++ monte_carlo/estimate_histogram_generator.R
@@ -4,43 +4,39 @@ library(rlang)
 library(gridExtra)
 library(ggeffects)
 
-# Make a single histogram plot using estimate data
-# an estimate name, a true value that is being estimated
-# and the root of the estimate and true value name
+
+#' Return a single histogram plot of estimates around the true value
+#' 
+#' @param est_dat data frame: estimates
+#' @param est_name string: name of estimate to be plotted 
+#' @param true_value data frame: true value 
+#' @param name_root string: root of column name that estimates and true value 
+#' share
+#' @return ggplot histogram of estimates around true value
+
 make_plot <- function(est_dat
                       , est_name
                       , true_value
                       , name_root){
-  plot <- est_dat %>%
-    ggplot(aes_string(x =est_name )) +
+    est_dat %>%
+    ggplot(aes_string(x = est_name)) +
     geom_histogram(color = 'black'
                    , alpha = 0.9) +
+    # vertical line represents true value
     geom_vline(xintercept = pull(true_value)
                , color = 'red'
                , alpha = 1) +
     ylab('Frequency') +
     xlab(str_to_title(name_root)) +
     theme_cowplot()
-  return(plot)
 }
 
-# combine plots into a grid
-combine_plots <- function(plot_list){
- plot_grid(plotlist = plot_list)
-}
-
-generate_histograms <- function(estimates_logit, true_values_logit){
-  plot_list <- generate_plots(estimates_logit, true_values_logit)
-  combine_plots(plot_list)
-}
-
-# take in an unspecified number of estimates and 
-# plot a histogram of each as well as the true value 
-# combine them on the same plot
-
-# what is the identifying feature? 
-# it's the column (estimate) name
-# apply across columns with est in the name 
+#' Return a plotted grid of histograms representing the spread of 
+#' estimates and true values for a series of coefficients (or other estimates) 
+#' 
+#' @param estimates_logit data frame of regression coefficient estimates 
+#' @param true_values_logit data frame of corresponding true values for coefficients
+#' @return list of histogram plots showing estimate spread around the true value  
 
 # takes in dataframe of estimates, returns a list of histogram graphics objects
 generate_plots <- function(estimates_logit, true_values_logit) {
@@ -61,7 +57,19 @@ generate_plots <- function(estimates_logit, true_values_logit) {
     # store plot in list
     graphics[[name_root]] <- make_plot(this_est, name, true_value, name_root)
   }
-  return(graphics)
+  # return graphics
+  graphics
 }
-  
-# map_if(estimates_logit, str_detect(colnames(estimates_logit), '_est'), hist)
+
+
+
+# combine plots into a grid
+combine_plots <- function(plot_list){
+ plot_grid(plotlist = plot_list)
+}
+
+generate_histograms <- function(estimates_logit, true_values_logit){
+  plot_list <- generate_plots(estimates_logit, true_values_logit)
+  combine_plots(plot_list)
+}
+
