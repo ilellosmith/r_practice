a18548487c41ff94262b948c3c476839c96d550c
Isaac LS <ilellosmith@gmail.com>
2020-07-05 15:09:04 -0400

    Adding new exercises, time comparison

--- grinstead_snell_probability/01_chapter_1_exercises.R
+++ grinstead_snell_probability/01_chapter_1_exercises.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(microbenchmark)
+library(tictoc)
 # This script contains function definitions to complete the exercises in 
 # Grinstead and Snell's "Introduction to Probability" (2006)
 
@@ -70,7 +72,7 @@ mc_prop %>%
 
 # Exercise 3 ----
 #' Simulates the roll of three dice n times,
-#' and tracks the proportion of times that their sum
+#' and tracks the number of times that their sum
 #' is sum1 and sum2
 #'
 galileo_gamble <- function(n, sum1 = 9, sum2 = 10){
@@ -83,3 +85,153 @@ galileo_gamble <- function(n, sum1 = 9, sum2 = 10){
   cbind(sum1_n, sum1, sum2_n, sum2) %>% 
     as_tibble() 
 }
+# Exercise 4 ----
+
+#' Plays many games of raquetball, and returns your average 
+#' probability of winning. 
+#' 
+#' Rules of the game: when you serve
+#' you have a "serve" chance of winning the point. 
+#' When you return, you have a "return" chance of winning back the serve
+#' A player wins a point only on their serve. A player wins the game
+#' when they reach win points
+#' 
+#' @param n_games - number of games to simulate
+#' @param serve - your probability of winning a point when serving
+#' @param return - your probability of winning a point when returning
+#' @param win - number of points to win
+#' @param last_win - who starts serving. If 1 then you start serving
+#' 
+play_many_games <- function(n_games = 100, serve =0.6, return =0.5, win = 21, last_win = 1){
+  outcomes <- replicate(n_games, play_game())
+  mean(outcomes)
+}
+
+#' Plays a single game of raquetball, where when you serve
+#' you have a "serve" chance of winning the point. 
+#' When you return, you have a "return" chance of winning back the serve
+#' A player wins a point only on their serve. A player wins the game
+#' when they reach win points
+#' 
+#' @param serve - your probability of winning a point when serving
+#' @param return - your probability of winning a point when returning
+#' @param win - number of points to win
+#' @param last_win - who starts serving. If 1 then you start serving
+#' 
+play_game <- function(serve =0.6, return =0.5, win = 21, last_win = 1){
+  # Initialize game
+  my_score <- 0
+  opp_score <- 0
+  # Play game to 21
+  while(my_score < 21 & opp_score < 21) {
+    # If won last point, you serve
+    if (last_win == 1){
+      # Win prob = serve probability
+      point <- rbinom(1,1,serve)
+      # If you lose, they serve
+      if (point == 0){
+        last_win <- 0
+        # Otherwise you win, your score goes up, you continue to serve
+      } else {
+        my_score <- my_score + 1
+      }
+      # If you lost last point, opponent serves  
+    } else {
+      # Win prob = return probability
+      point <- rbinom(1,1, return)
+      # If you win, you get to serve again
+      if (point == 1){
+        last_win <- 1
+        # If you lose, opponents score goes up, they continue to serve
+      } else {
+        opp_score <- opp_score + 1
+      }
+    }
+  }
+  # calculate whether you won the game
+  ifelse(my_score == win, 1, 0)
+}
+# Exercise 5 ----
+
+#' Simulates the roll of three dice n times,
+#' and tracks whether at least one triple-six 
+#' was rolled 
+#' 
+#' @param n number of rolls of three dice
+triple_six <- function(n){
+  # Simulate n sums from n rolls of three die
+  roll_sums <- replicate(n, sum(sample(1:6, 3, replace = T)))
+  # Count whether at least one triple six was rolled 
+  any(ifelse(roll_sums == 18, T,F))
+}
+
+#' Simulates n_trials of rolling three dice n times,
+#' and calculates the probability of at least one 
+#' triple-six when three dice are rolled n times
+#' 
+#' NB: this function operates by filling in a prefab matrix
+#' 
+#' @param n_trials - number of trials to simulate at each level of n_rolls
+#' @param n_rolls - number of rolls of three dice
+mc_triple_six <- function(n_trials, n_rolls){
+  dat <- matrix(, nrow = n_trials, ncol = length(n_rolls))
+  # Run experiments
+  for (i in 1:n_trials){
+    dat[i,] <- sapply(n_rolls, triple_six)
+  }  
+  # Get proportion with triple sixes at each n_rolls level
+  mc_prop <- colMeans(dat) %>% 
+    enframe()
+  colnames(mc_prop) <- c('n_rolls', 'prop')
+  # Plot proportion with triple sixes at each n_rolls level against 0.5
+  mc_prop %>%
+    ggplot(aes(x = n_rolls, y = prop)) +
+    geom_point() +
+    geom_hline(yintercept = 0.5, color = 'red') +
+    theme_minimal() 
+}
+
+#' Simulates n_trials of rolling three dice n times,
+#' and calculates the probability of at least one 
+#' triple-six when three dice are rolled n times
+#' 
+#' NB: this function operates by cbinding each trial
+#' 
+#' @param n_trials - number of trials to simulate at each level of n_rolls
+#' @param n_rolls - number of rolls of three dice
+mc_triple_six_cbind <- function(n_trials, n_rolls){
+  dat <- vector()
+  # Run experiments
+  for (i in 1:n_trials){
+    trial <- sapply(n_rolls, triple_six)
+    dat <- cbind(dat,trial)
+  }  
+  # Get proportion with triple sixes at each n_rolls level
+  mc_prop <- rowMeans(dat) %>% 
+    enframe()
+  colnames(mc_prop) <- c('n_rolls', 'prop')
+  # Plot proportion with triple sixes at each n_rolls level against 0.5
+  mc_prop %>%
+    ggplot(aes(x = n_rolls, y = prop)) +
+    geom_point() +
+    geom_hline(yintercept = 0.5, color = 'red') +
+    theme_minimal() 
+}
+
+# My simulations take a while to run. Using autobenchmark to compare 
+time_compare <- microbenchmark::microbenchmark(
+  'matrix' = {
+    mc_triple_six(100, c(150:200))
+  },
+  'cbind' = {
+    mc_triple_six_cbind(100, c(150:200))
+  }
+)
+autoplot(time_compare)
+
+# Looks like cbind is actually more efficient 
+# Need to figure out another way to speed this up
+
+
+
+
